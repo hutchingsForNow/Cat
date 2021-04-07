29544ca67a5550694cbc66d2d15aec72dc7108ba
Reagan Hutchings <reaganbh@byu.edu>
2021-04-06 22:40:07 -0600

    starting over with anime

--- catScript.js
+++ catScript.js
@@ -1,67 +1,20 @@
-new Vue({ 
-    el: '#app',
-    vuetify: new Vuetify(),
-    data: {
-      images: [],
-      order:'Desc',
-      page: 100,
-      limit: 3,
-      pagination_count: 0, //default until we get a result with the 'Pagination-Count' header in the response
-    },
-    created(){
-        this.getImages();
-    } ,
-    watch: {
-        // if the user changes any of these values, then make a new request to the API
-        page: function()
-        {
-            this.getImages();
-        },
-        limit: function()
-        {
-            this.getImages();
-        },
-        order: function()
-        {
-            this.getImages();
-        }
-    },
-    computed:{
+//for trace api which accepts scans of animes and returns info on them.
+var img = document.querySelector("img"); // select image from DOM
+var canvas = document.createElement("canvas");
+canvas.width = img.naturalWidth;
+canvas.height = img.naturalHeight;
+var ctx = canvas.getContext("2d");
+ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
-        getNumPages: function()
-        {
-        return Math.floor(this.pagination_count / this.limit) | 0;
-        }
-    },
-    methods:{
-
-        async nextBtn()
-        {
-            this.page++;
-            await this.getImages();
-        },
-        async getImages()
-        {
-            try{
-                axios.defaults.headers.common['x-api-key'] = "0d6c5d29-0b74-4c21-a06b-5ff1681debab" // Replace this with your API Key
-
-                let query_params = {
-                    limit: this.limit,
-                    order: this.order,
-                    page: this.page-1,
-                }
-
-                let response = await axios.get('https://api.thecatapi.com/v1/images/search', { params: query_params } ) 
-                
-                this.pagination_count = response.headers['pagination-count'];
-                this.images = response.data 
-
-                console.log("-- ("+this.images.length +") Images from TheCatAPI.com")
-                console.log( this.pagination_count ,'images available for this query.')
-                
-            }catch(err){
-                console.log(err)
-            }
-        }
-    }
+fetch("https://trace.moe/api/search", {
+  method: "POST",
+  body: JSON.stringify({ image: canvas.toDataURL("image/jpeg", 0.8) }),
+  headers: { "Content-Type": "application/json" },
 })
+  .then((res) => res.json())
+  .then((result) => {
+    console.log(result);
+  });
+
+//for search anime api jankin
+fetch("https://api.jikan.moe/v3")
