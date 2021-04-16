4e987fa052f78d6e802ab65650cf3c558d7c3e72
Reagan Hutchings <reaganbh@byu.edu>
2021-04-15 23:13:23 -0600

    finished this, it is now a joke api website

--- catScript.js
+++ catScript.js
@@ -1,20 +1,57 @@
-//for trace api which accepts scans of animes and returns info on them.
-var img = document.querySelector("img"); // select image from DOM
-var canvas = document.createElement("canvas");
-canvas.width = img.naturalWidth;
-canvas.height = img.naturalHeight;
-var ctx = canvas.getContext("2d");
-ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+//joke of the day
+document.getElementById("jokeOfDaySubmit").addEventListener("click", function(event) {
+  event.preventDefault();
+  //api
+  const url = "https://api.jokes.one/jod?category=animal";
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+          console.log(json);
+          let results = "";
+          results += "<h3>" + json.contents.jokes[0].joke.title + "</h3>";
+          results += "<p>" + json.contents.jokes[0].joke.text + "</p><br/>";
+          document.getElementById("jokeOfDayResults").innerHTML = results;
+    });
+});
 
-fetch("https://trace.moe/api/search", {
-  method: "POST",
-  body: JSON.stringify({ image: canvas.toDataURL("image/jpeg", 0.8) }),
-  headers: { "Content-Type": "application/json" },
-})
-  .then((res) => res.json())
-  .then((result) => {
-    console.log(result);
-  });
 
-//for search anime api jankin
-fetch("https://api.jikan.moe/v3")
+//Chuck Noris Button Joke
+document.getElementById("norisSubmit").addEventListener("click", function(event) {
+  event.preventDefault();
+  const value = document.getElementById("norisNumJokes").value;
+  if (value === "")
+    return;
+  console.log(value);
+  //api
+  const url = "http://api.icndb.com/jokes/random/" + value;
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+          console.log(json);
+          let results = "";
+          for(let i = 0; i < value; ++i){
+            results += "<p>" + json.value[i].joke + "</p><br/>";
+          }
+          document.getElementById("NorisResults").innerHTML = results;
+    });
+});
+
+
+//search for a joke
+document.getElementById("jokeSubmit").addEventListener("click", function(event) {
+  event.preventDefault();
+  // api
+  const url = "https://official-joke-api.appspot.com/random_joke"; 
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+          console.log(json);
+          let results = "";
+          results += "<p>" + json.setup + "</p>";
+          results += "<p>" + json.punchline + "</p><br/>";
+          document.getElementById("searchResults").innerHTML = results;
+    });
+});
